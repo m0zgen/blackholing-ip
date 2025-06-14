fa5fb268f9fa26dbeb71735b4cf992fc81b4bfb1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2025-06-14 19:54:21 +0500

    + + Delete IP from blackholing

--- blip.sh
+++ blip.sh
@@ -2,26 +2,12 @@
 # Author: Yevgeniy Goncharov aka xck, http://sys-adm.in
 # Create blackhole route for IP list or single IP
 
-# Sys env / paths / etc
-# -------------------------------------------------------------------------------------------\
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
-# Variables
-# -------------------------------------------------------------------------------------------\
-# Gateway and interface for blackhole route
 GATEWAY="127.0.0.1"
 INTERFACE="lo"
 
-# Functions
-# -------------------------------------------------------------------------------------------\
-# Check for root
-# if [ "$(id -u)" != "0" ]; then
-#   echo "For run this script you need root access."
-#   exit 1
-# fi
-
-# Check file exists function
 check_file() {
   if [ ! -f "$1" ]; then
     echo "File '$1' not found."
@@ -29,7 +15,6 @@ check_file() {
   fi
 }
 
-# Check if ip command exists and return true or false
 check_ip_command() {
   if [ -x "$(command -v ip)" ]; then
     return 0
@@ -38,55 +23,75 @@ check_ip_command() {
   fi
 }
 
-# Add to route function
 add_route() {
-  # Check for IP
-  if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    local ip="$1"
+  local ip="$1"
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || \
+     [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}$ ]]; then
     if check_ip_command; then
       ip route add ${ip} via ${GATEWAY} dev ${INTERFACE}
       echo "Added route for $ip"
     fi
-    # echo "Added route for $ip"
-  elif [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}$ ]]; then
-    local ip="$1"
-    if check_ip_command; then
-      ip route add ${ip} via ${GATEWAY} dev ${INTERFACE}
-      echo "Added route for $ip"
-    fi
-    # echo "Added route for $ip"
   else
-    echo "String '$1' is not a valid IP address."
+    echo "String '$ip' is not a valid IP address."
   fi
 }
 
-# Read IP list from file and pass to add_route function
+del_route() {
+  local ip="$1"
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || \
+     [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}$ ]]; then
+    if check_ip_command; then
+      ip route del ${ip} via ${GATEWAY} dev ${INTERFACE}
+      echo "Deleted route for $ip"
+    fi
+  else
+    echo "String '$ip' is not a valid IP address."
+  fi
+}
+
 read_ip_list() {
   local IP_LIST="$1"
   check_file "$IP_LIST"
-
   while IFS= read -r line; do
     add_route "$line"
   done < "$IP_LIST"
 }
 
-# Check for arguments --file or --ip
+delete_ip_list() {
+  local IP_LIST="$1"
+  check_file "$IP_LIST"
+  while IFS= read -r line; do
+    del_route "$line"
+  done < "$IP_LIST"
+}
+
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 --file <file_with_ip_list> or $0 --ip <ip_address>"
+  echo "Usage:"
+  echo "  $0 --file <file_with_ip_list>         # Add routes from file"
+  echo "  $0 --ip <ip_address>                  # Add single IP"
+  echo "  $0 --delete-file <file_with_ip_list>  # Delete routes from file"
+  echo "  $0 --delete-ip <ip_address>           # Delete single IP"
   exit 1
 fi
 
-# Processing arguments with while
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --file)
       shift
       IP_LIST="$1"
-      read_ip_list "$IP_LIST"  
+      read_ip_list "$IP_LIST"
       ;;
     --ip)
       shift
       add_route "$1"
+      ;;
+    --delete-ip)
+      shift
+      del_route "$1"
+      ;;
+    --delete-file)
+      shift
+      delete_ip_list "$1"
       ;;
     *)
       echo "Unknown argument: $1"
